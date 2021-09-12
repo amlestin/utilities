b1d2c13477c2a70cee64495891d672c794e1473c
Arnold Lestin <amlestin@gmail.com>
2021-09-12 00:13:32 -0400

    first commit

---
+++ file.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("killall -9 vim")
---
+++ salcalc.py
@@ -0,0 +1,9 @@
+def calc_sal(hourly_rate, hours = 40):
+	weeks = 52
+	return hours*weeks*hourly_rate
+
+
+hourly = int(input("Enter the number hourly rate of the job: "))
+salary = calc_sal(hourly)	
+
+print(f"An hourly rate of ${hourly}/hr equates to an approximate annual salary = ${salary}")
