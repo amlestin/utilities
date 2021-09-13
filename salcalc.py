ac825455f2b5a52d122caf496f79dc61a79c689a
Arnold Lestin <amlestin@gmail.com>
2021-09-13 10:15:00 -0400

    Added command line arg feature

--- salcalc.py
+++ salcalc.py
@@ -1,9 +1,11 @@
+import sys
+
 def calc_sal(hourly_rate, hours = 40):
 	weeks = 52
 	return hours*weeks*hourly_rate
 
+hourly_rate = int(sys.argv[1])
+#hourly = int(input("Enter the number hourly rate of the job: "))
+salary = calc_sal(hourly_rate)	
 
-hourly = int(input("Enter the number hourly rate of the job: "))
-salary = calc_sal(hourly)	
-
-print(f"An hourly rate of ${hourly}/hr equates to an approximate annual salary of ${salary}")
+print(f"An hourly rate of ${hourly_rate}/hr equates to an approximate annual salary of ${salary}")
