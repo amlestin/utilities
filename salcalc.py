77c78162e0dca121c14a9ed03a78b7ac7c11a7a7
Arnold Lestin <amlestin@gmail.com>
2021-09-13 10:17:35 -0400

    Added a fallback for manual entry

--- salcalc.py
+++ salcalc.py
@@ -4,8 +4,11 @@ def calc_sal(hourly_rate, hours = 40):
 	weeks = 52
 	return hours*weeks*hourly_rate
 
-hourly_rate = int(sys.argv[1])
-#hourly = int(input("Enter the number hourly rate of the job: "))
-salary = calc_sal(hourly_rate)	
+try:
+	hourly_rate = int(sys.argv[1])
+except IndexError:
+	hourly_rate = int(input("Enter the number hourly rate of the job: "))
+finally:
+	salary = calc_sal(hourly_rate)	
 
 print(f"An hourly rate of ${hourly_rate}/hr equates to an approximate annual salary of ${salary}")
