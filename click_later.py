aa79e4fb48eff18d3e9b41b6a1a2e7297121ad27
Arnold Lestin <amlestin@gmail.com>
2021-09-15 11:57:07 -0400

    Added command line delay arg

--- click_later.py
+++ click_later.py
@@ -1,9 +1,14 @@
 import time
+import os
+import sys
 from pynput.mouse import Button, Controller
 
 mouse = Controller()
 
-delay = 1
+if len(sys.argv) < 2:
+    delay = 1
+else:
+    delay = int(sys.argv[1])
 
 # Read pointer position
 print('The current pointer position is {0}'.format(
