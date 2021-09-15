8f319d0220a366d2deafb11fc12e24716a596cfe
Arnold Lestin <amlestin@gmail.com>
2021-09-15 11:55:22 -0400

    Added delay variable

--- click_later.py
+++ click_later.py
@@ -3,8 +3,11 @@ from pynput.mouse import Button, Controller
 
 mouse = Controller()
 
+delay = 1
+
 # Read pointer position
 print('The current pointer position is {0}'.format(
     mouse.position))
 
+time.sleep(delay)
 mouse.click(Button.left, 1)
