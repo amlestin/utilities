8b3d6d12a296466cf3f9f67b7d6509f409556c4b
Arnold Lestin <amlestin@gmail.com>
2022-03-14 18:25:08 -0500

    Modularized code

--- notes_exporter.py
+++ notes_exporter.py
@@ -13,9 +13,6 @@ else:
 # Delay between keyboard/mouse inputs
 INPUT_DELAY = 1
 
-# init mouse and keyboard controllers
-my_mouse = mouse.Controller()
-my_keyboard = keyboard.Controller()
 
 # Press and release cmd+a
 def select_all_text(my_keyboard):
@@ -39,32 +36,44 @@ def create_notes_dir():
     except OSError as e:
         print(f'Attempted to create notes dir that already exists: {e}')
 
-# Save pointer position
-notes_pos = my_mouse.position
-print(f'The Notes pointer position is {notes_pos}')
-print(f'Clicking in: {START_DELAY} seconds!')
-time.sleep(START_DELAY)
+def save_cur_note(my_mouse, my_keyboard):
+    # Click on notes window to focus it
+    my_mouse.click(mouse.Button.left, 2)
+    time.sleep(INPUT_DELAY)
 
-# Click on notes window to focus it
-my_mouse.click(mouse.Button.left, 2)
-time.sleep(INPUT_DELAY)
+    # Select all text in the notes window and copy it to keyboard
+    select_all_text(my_keyboard)
+    time.sleep(INPUT_DELAY)
+    copy_text(my_keyboard)
 
-# Select all text in the notes window and copy it to keyboard
-select_all_text(my_keyboard)
-time.sleep(INPUT_DELAY)
-copy_text(my_keyboard)
+    # Extract notes text from clipboard and separate to header, body
+    text = pyperclip.paste()
+    text_lines = text.split("\n")
+    header = text_lines[0].rstrip()
+    body = '\n'.join(text_lines[1:])
 
-# Extract notes text from clipboard and separate to header, body
-text = pyperclip.paste()
-text_lines = text.split("\n")
-header = text_lines[0].rstrip()
-body = '\n'.join(text_lines[1:])
+    # Log processed text
+    print(header)
+    print(body)
 
-# Log processed text
-print(header)
-print(body)
+    # Create notes dir if not exists and write a text file for the current note
+    create_notes_dir()
+    with open(f'notes/{header}.txt', 'w') as f:
+        f.write(body)
 
-# Create notes dir if not exists and write a text file for the current note
-create_notes_dir()
-with open(f'notes/{header}.txt', 'w') as f:
-    f.write(body)
+
+def main():
+    # init mouse and keyboard controllers
+    my_mouse = mouse.Controller()
+    my_keyboard = keyboard.Controller()
+
+    # Save pointer position
+    notes_pos = my_mouse.position
+    print(f'The Notes pointer position is {notes_pos}')
+    print(f'Clicking in: {START_DELAY} seconds!')
+    time.sleep(START_DELAY)
+
+    save_cur_note(my_mouse, my_keyboard)
+
+
+main()
