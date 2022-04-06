9415620485fccd2eaaea4ada62cc618332069682
Arnold Lestin <amlestin@gmail.com>
2022-04-05 21:03:12 -0500

    Found cwd

--- movie_picker/movie_picker.go
+++ movie_picker/movie_picker.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
+	current_dir, err := os.Getwd()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Scanning foler: " + current_dir + "\n")
+
+	// TODO: List files
+	// TODO: Pick a random value in list if it ends in a movie fmt
 	chosen_movie := "test"
 	fmt.Println("Your next movie is: " + chosen_movie)
 }
