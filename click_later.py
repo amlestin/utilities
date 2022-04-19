ed97b9b5f42d8ff8fbdfb6e9cd792db8c5d69c33
Arnold Lestin <amlestin@gmail.com>
2022-04-18 21:39:46 -0500

    implemented TODO

--- click_later.py
+++ click_later.py
@@ -3,18 +3,25 @@ import os
 import sys
 from pynput.mouse import Button, Controller
 
+from notes_exporter.src.notes_exporter import START_DELAY
+
+START_DELAY = 3
+
 mouse = Controller()
 
 if len(sys.argv) < 2:
-    delay = 1
+    SLEEP_DUR = 1
 else:
-    delay = int(sys.argv[1])
+    SLEEP_DUR = int(sys.argv[1])
 
+time.sleep(START_DELAY)
 # Read pointer position
 print(f'The current pointer position is {mouse.position}')
-#TODO: Save current pointer position for use after delay
+# Save pointer position
+mouse_pos = mouse.position
 
-print(f'Clicking in: {delay} seconds!')
-time.sleep(delay)
+print(f'Clicking in: {SLEEP_DUR} seconds!')
+time.sleep(SLEEP_DUR)
 
+mouse.position = mouse_pos
 mouse.click(Button.left, 1)
